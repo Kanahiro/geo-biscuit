2aab068391b9053cf29cb6aa9cc35cf4b8bed2f6
Kanahiro <mediterranean1769@gmail.com>
2021-04-20 23:09:27 +0900

    add bin module

---
+++ bin/index.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+
+import { ArgumentParser, HelpFormatter } from 'argparse';
+import { version } from '../package.json';
+import { FeatureCollection } from 'geojson';
+
+import { getBiscuitCutter } from '../src/index';
+
+const parser = new ArgumentParser({
+    add_help: true,
+    formatter_class: HelpFormatter,
+});
+parser.add_argument('-v', '--version', { action: 'version', version });
+parser.add_argument('-i', '--input', { help: 'input geojson path' });
+parser.add_argument('-o', '--output', { help: 'output geojson path' });
+parser.add_argument('--shape', {
+    help: 'biscuit-cutter name, triangle, square...',
+});
+parser.add_argument('--scaler', {
+    help: 'polygon size multiplier, default to 1.0',
+});
+const args = parser.parse_args();
+
+const input = JSON.parse(
+    fs.readFileSync(args.input).toString(),
+) as FeatureCollection;
+
+const cutter = getBiscuitCutter(args.shape, { scaler: args.scaler });
+const biscuits = cutter(input);
+
+fs.writeFileSync(args.output, JSON.stringify(biscuits));
